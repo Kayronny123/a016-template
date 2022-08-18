8af564cac2ceb407a21ea1a3cbbd36023cd8af94
rogerio kayronny <kayronnysales@gmail.com>
2022-08-18 14:01:20 -0300

    Aula II laços - for.. in e for.. of

--- script.js
+++ script.js
@@ -34,4 +34,80 @@ const filmes = [
   },
 ]
 
+
+
+
+  for(let sequencia2 of filmes){
+    sequencia2 = (`O nome do filme é: ${sequencia2.titulo} foi lançado em: ${sequencia2.ano} dirigido por: ${sequencia2.diretor} e tem como elenco: ${sequencia2.elenco}`)
+        console.log(sequencia2)
+  }
+
+  
+
+
+
 // escreva seu código abaixo 👇🏻
+// exercicio 1
+// const jogos = [[3, 4, 12, 23, 34, 45],
+// [4, 5, 8, 11, 29, 31],
+// [2, 4, 6, 8, 32, 34],
+// [5, 8, 13, 25, 36, 62],
+// ]
+
+// if (jogos.length === 4) {
+//   // escrevendo codigo
+//   for (let i = 0; i < jogos.length; i++) {
+//     // percorrendo o array principal
+//     let sorteio = `Sorteio ${i + 1}:`
+//     for (let j = 0; j < jogos[i].length; j++) {
+//       // percorre o array secundario
+//       sorteio += `${jogos[i][j]}`
+//     }
+//     console.log(sorteio)
+//   }
+// } else {
+//   console.log(' é necessario alterar o numero de itens o array')
+// }
+// exercicio2
+// const jogos = [[3, 4, 12, 23, 34, 45],
+// [4, 5, 8, 11, 29, 31],
+// [2, 4, 6, 8, 32, 34],
+// [5, 8, 13, 25, 36, 62],
+// ]
+
+// if (jogos.length === 4) {
+//   // escrevendo codigo
+//   for (let i in jogos) {
+//     // percorrendo o array principal
+//     let sorteio = `Sorteio ${ Number ( i )  +  1} : `
+//     for (let j = 0; j < jogos[i].length; j++) {
+//       // percorre o array secundario
+//       sorteio += ` ${jogos [i][j]}`
+//     }
+//     console.log( sorteio ) 
+//   }
+// } else {
+//   console.log(' é necessario alterar o numero de itens o array')
+// }
+
+// exercicio 3
+// const jogos = [[3, 4, 12, 23, 34, 45],
+// [4, 5, 8, 11, 29, 31],
+// [2, 4, 6, 8, 32, 34],
+// [5, 8, 13, 25, 36, 62],
+// ]
+
+// if (jogos.length === 4) {
+//   // escrevendo codigo
+//   for (let i in jogos) {
+//     // percorrendo o array principal
+//     let sorteio = `Sorteio ${ Number ( i ) + 1}:`
+//     for (let j of jogos [ i ]) {
+//       // percorre o array secundario
+//       sorteio += ` ${[j]}`
+//     }
+//     console.log( sorteio )
+//   }
+// } else {
+//   console.log(' é necessario alterar o numero de itens o array')
+// }
